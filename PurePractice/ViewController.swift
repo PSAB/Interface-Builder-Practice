7c8aeea4928db667a003c70232240a3ca8e62c6c
Pavan Sabnis <pvnsbns@gmail.com>
2018-01-25 13:31:15 -0800

    Initial Commit

---
+++ PurePractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PurePractice
+//
+//  Created by Pavan Sabnis on 1/25/18.
+//  Copyright © 2018 Pavan Sabnis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
